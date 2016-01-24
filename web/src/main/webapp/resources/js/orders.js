525f279b15a1f2160b70aad46092334b512ba0bd
TenebrisFx <bohdandarkzolochiv@gmail.com>
2016-01-24 23:09:35 +0200

    Orders page modified

--- web/src/main/webapp/resources/js/orders.js
+++ web/src/main/webapp/resources/js/orders.js
@@ -1,4 +1,38 @@
+var hourlySlider, fixedSlider;
+var filterOpen = false;
+
 $(document).ready(function() {
-	
-	$('#filter_panel')
+	$("#hourlyPayTypeSlider").slider({
+		id : "hourlySlider",
+		min : 0,
+		max : 10,
+		range : true
+	}).slider('disable');
+	$("#fixedPayTypeSlider").slider({
+		id : "fixedSlider",
+		min : 0,
+		max : 10,
+		range : true
+	}).slider('disable');
+
+	$('.checkbox > input[type="checkbox"]').change(function() {
+		var parent = $(this).parent().parent();
+		if (this.checked) {
+			$(parent).find('input[id$="PayTypeSlider"]').slider("enable");
+		} else {
+			$(parent).find('input[id$="PayTypeSlider"]').slider("disable");
+		}
+	});
+
+	$("#timeZoneSelect").select2();
+
+	$('#filterHeader').click(function() {
+		if (filterOpen) {
+			$('#order_filter').slideUp();
+			filterOpen = false;
+		} else {
+			$('#order_filter').slideDown();
+			filterOpen = true;
+		}
+	});
 });
